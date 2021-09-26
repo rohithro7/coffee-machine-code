fb90e0381a50ecdf2e130ed1b6b82402b929b438
rohithro7 <52529570+rohithro7@users.noreply.github.com>
2021-09-26 10:43:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-print("Menu")
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+flag = True
+option = menu.get_items()
+
+while flag:
+    choice = input(f"What would like to have? {option}: ")
+    if choice == "off":
+        flag = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
